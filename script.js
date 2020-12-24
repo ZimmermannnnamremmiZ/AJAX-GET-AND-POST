0b546b8434660477648d113f3011de2f38db6fc8
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2020-12-24 17:10:18 +0300

    2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button');
-
+const countainer = document.querySelector('.countainer');
 
 // т.к. действие асинхронное, создаем callback
 function getPosts(callback) {
@@ -11,7 +11,6 @@ function getPosts(callback) {
     xhr.addEventListener("load", () => {
         // перевод ответа от сервера в массив
         const responce = JSON.parse(xhr.responseText);
-        console.log(responce);
         callback(responce);
     });
 
@@ -30,6 +29,23 @@ function getPosts(callback) {
 // повесили обработчик событий на кнопку (при клике на которую вызовется getPosts)
 btn.addEventListener("click", (e) => {
     getPosts(response => {
-        console.log(response)
+        const fragment = document.createDocumentFragment();
+        response.forEach(post => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            const cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+            const title = document.createElement("h5");
+            title.classList.add("card-title");
+            title.textContent = post.title;
+            const article = document.createElement("p");
+            article.classList.add("card-text");
+            article.textContent = post.body;
+            cardBody.appendChild(title);
+            cardBody.appendChild(article);
+            card.appendChild(cardBody);
+            fragment.appendChild(card);
+        });
+        countainer.appendChild(fragment);
     });
 });
