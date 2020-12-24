7b266c33e7b7ce56213c3672c9a9cb12a005edb5
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2020-12-24 16:48:56 +0300

    1

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-function getPosts() {
+const btn = document.querySelector('button');
+
+
+// т.к. действие асинхронное, создаем callback
+function getPosts(callback) {
     // некие настройки будущего запроса
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 
     // подписываемся на событие загрузки и получения данных от сервера     
     xhr.addEventListener("load", () => {
-
+        // перевод ответа от сервера в массив
+        const responce = JSON.parse(xhr.responseText);
+        console.log(responce);
+        callback(responce);
     });
+
+
 
     //подписка на события Error
     xhr.addEventListener("error", () => {
-        console.log('error', )
+        console.log('error')
     })
 
     // отправка запроса
     xhr.send();
 }
+
+// получение ответа от сервера вне функции
+// повесили обработчик событий на кнопку (при клике на которую вызовется getPosts)
+btn.addEventListener("click", (e) => {
+    getPosts(response => {
+        console.log(response)
+    });
+});
