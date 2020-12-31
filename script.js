cfa6c197c8ce0cebd7ec23833db88d1dc25b6633
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2020-12-31 23:01:23 +0300

    :)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const btn = document.querySelector('button');
-const countainer = document.querySelector('.countainer');
+const btn = document.querySelector('.btn-get-posts');
+const btnAddPost = document.querySelector('.btn-add-post');
+const container = document.querySelector('.container');
 
 // т.к. действие асинхронное, создаем callback
 function getPosts(callback) {
@@ -25,30 +26,66 @@ function getPosts(callback) {
     xhr.send();
 }
 
+function createPost(body, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");     
+    xhr.addEventListener("load", () => {
+        const responce = JSON.parse(xhr.responseText);
+        callback(responce);
+    });
+
+    // без этого придет только id
+xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8")
+
+    xhr.addEventListener("error", () => {
+        console.log('error')
+    })
+    xhr.send(JSON.stringify(body));
+}
+
+// вынесли 'card' в отдельную функцию
+function cardTemplate(post) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.textContent = post.title;
+    const article = document.createElement("p");
+    article.classList.add("card-text");
+    article.textContent = post.body;
+    cardBody.appendChild(title);
+    cardBody.appendChild(article);
+    card.appendChild(cardBody);
+    return card;
+}
+
+
 function renderPosts(response) {
 
     const fragment = document.createDocumentFragment();
     response.forEach(post => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        const title = document.createElement("h5");
-        title.classList.add("card-title");
-        title.textContent = post.title;
-        const article = document.createElement("p");
-        article.classList.add("card-text");
-        article.textContent = post.body;
-        cardBody.appendChild(title);
-        cardBody.appendChild(article);
-        card.appendChild(cardBody);
+        const card = cardTemplate(post);
         fragment.appendChild(card);
     });
-    countainer.appendChild(fragment);
+    container.appendChild(fragment);
 }
 
 // получение ответа от сервера вне функции
 // повесили обработчик событий на кнопку (при клике на которую вызовется getPosts)
 btn.addEventListener("click", (e) => {
     getPosts(renderPosts);
+});
+
+btnAddPost.addEventListener("click", e => {
+    const newPost = {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      };
+    createPost(newPost, responce => {
+        const card = cardTemplate(responce);
+        container.insertAdjacentElement("afterbegin", card);
+    });
 });
